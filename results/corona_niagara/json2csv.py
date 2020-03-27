77aece9b726612f8627b5f6a75925983aaf4134e
HilariousJi <parker.wang210@gmail.com>
2020-03-27 12:31:23 -0400

    Moved test cases to examples

--- results/corona_niagara/json2csv.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_json (r'C:\Users\Ji\Dropbox\COSC 3P99\results\corona_niagara\userprofiles_test.json')
-df.to_csv (r'C:\Users\Ji\Dropbox\COSC 3P99\results\corona_niagara\userprofiles_test.csv', index = None)
