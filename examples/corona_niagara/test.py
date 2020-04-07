e54de73a27cad22c906a722bdb68b900fadba637
HilariousJi <parker.wang210@gmail.com>
2020-04-07 16:36:43 -0400

    Edited test.py

--- examples/corona_niagara/test.py
+++ examples/corona_niagara/test.py
@@ -7,13 +7,13 @@ c.User_full = True
 #c.Hide_output = True
 twint.run.Search(c)
 tweets = twint.output.tweets_list
-for t in tweets:
-    for u in t.username:
-        c = twint.Config()
-        c.Username = u
-        twint.run.Followers(c)
-        target_followers = twint.output.users_list
-        with open('followers.csv', 'w') as output:
+with open('followers.csv', 'w') as output:
+    for t in tweets:
+        for u in t.username:
+            c = twint.Config()
+            c.Username = u
+            twint.run.Followers(c)
+            target_followers = twint.output.users_list
             output.write('id,username,followers,following\n')
             for f in target_followers:
                 output.write('{},{},{},{}\n'.format(
