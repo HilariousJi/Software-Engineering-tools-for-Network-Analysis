ea8d5565434c78916c7d25f123b0fe2f743dbf2a
HilariousJi <parker.wang210@gmail.com>
2020-04-24 13:07:42 -0400

    Added scripts to search with locations

---
+++ examples/corona_niagara/tweets_by_location.py
@@ -0,0 +1,12 @@
+import twint
+
+c = twint.Config()
+c.Near = "St. Catharines"
+c.Search = "coronavirus OR COVID-19 OR cough OR fever"
+c.Store_object = True
+#c.Hide_output = True
+twint.run.Search(c)
+tweets = twint.output.tweets_list
+with open('tweets_location.csv', 'w') as output:
+    for t in tweets:
+        output.write('{},{}\n'.format(t.username, t.geo))
