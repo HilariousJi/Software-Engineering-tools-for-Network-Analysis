01e97661313607114302ca9e46411e6b4d1a01a7
HilariousJi <parker.wang210@gmail.com>
2020-08-10 14:29:51 -0400

    Fixed Scipt.js

--- scripts/Script.js
+++ scripts/Script.js
@@ -4,6 +4,8 @@ loadModule('/TraceCompass/View');
 loadModule('/TraceCompass/Trace');
 loadModule('/TraceCompass/Filters');
 
+
+//how many cpus do we have in the system !//fixme
 var thisTrace = getActiveTrace();
 var analysis = createScriptedAnalysis(thisTrace, "activetid.js");
 
@@ -11,7 +13,7 @@ if (thisTrace == null) {
     print("Trace is null");
     exit();
 }
-var ss = analysis.getStateSystem(false);
+// var ss = analysis.getStateSystem(false);
 var iter = analysis.getEventIterator();
 var event = null;
 // Write to a csv file, set in the scripting argument
@@ -36,19 +38,42 @@ function addTidToArray(arr, tid) {
     arr.push(tid);
 }
 
-var tids = new Array();
-var tid_list = new Array();
+var numCPU = 0;
 // Parse all events
 while (iter.hasNext()) {
     event = iter.next();
-    // Do something when the event is a sched_switch
+    if (event.getName() == "sched_switch") {//"lttng_statedump_process_state") {
+        if (numCPU < getEventFieldValue(event, "CPU")) {
+            numCPU = getEventFieldValue(event, "CPU")
+        }
+        // get ptid of each active thread and create a list/array/dictionary    
+    }
+}
+iter = analysis.getEventIterator();
+event = null;
+var tids = new Array(numCPU + 1)
+for (i = 0; i < numCPU + 1; i++) {
+    tids[i] = [];
+}
+var ntids = new Array(numCPU + 1)
+for (i = 0; i < numCPU + 1; i++) {
+    ntids[i] = [];
+}
+var tid_set = [];
+// var tid_list = new Array(numCPU);
+while (iter.hasNext()) {
+    event = iter.next();
     if (event.getName() == "sched_switch") {
         cpu = getEventFieldValue(event, "CPU");
         tid = getEventFieldValue(event, "tid");
-        if ((cpu != null) && (tid != null) && (tid != 0) && (tid != 1)) {
+        ntid = getEventFieldValue(event, "next_tid")
+        if ((cpu != null) && (tid != null) && (tid != 0) && (tid != 1) & (ntid != 0) & (ntid != 1)) {
             // Maintain a list of tids
-            tid_list.push(tid);
-            addTidToArray(tids, tid)
+            // tid_list[cpu].push(tid); //fixme
+            tids[cpu].push(tid)
+            ntids[cpu].push(ntid)
+            addTidToArray(tid_set, tid)
+            //}
         }
     }
 }
@@ -56,37 +81,38 @@ while (iter.hasNext()) {
 var source = []
 var target = []
 for (i = 0; i < tids.length; i++) {
-    source.push(tids[i])
-    target.push(tids[i])
+    for (j = 0; j < tids[i].length; j++) {
+        source.push(tids[i][j])
+        target.push(ntids[i][j])
+    }
 }
 var nodeMatrix = {}
 // Populate the matrix with 0's
-for (var i = 0; i < source.length; i++) {
-    var s_tid = source[i];
+
+for (var i = 0; i < tid_set.length; i++) {
+    var s_tid = tid_set[i];
     if (s_tid in nodeMatrix == false) {
         nodeMatrix[s_tid] = {}; // must initialize the sub-object, otherwise will get 'undefined' errors
     }
-
-    for (var j = 0; j < target.length; j++) {
-        var t_tid = target[j];
+    for (var j = 0; j < tid_set.length; j++) {
+        var t_tid = tid_set[j];
         nodeMatrix[s_tid][t_tid] = 0;
     }
 }
-var iniRank = 1 / tids.length
+
+var iniRank = 1 / tid_set.length
 // Write to edgelist.csv
-for (i = 0; i < tid_list.length - 1; i++) {
+for (i = 0; i < source.length; i++) {
     if (bufferedWriter == null) {
-        print(tid_list[i])
+        print(source[i])
     } else {
-        if (tid_list[i] != tid_list[i + 1]) {
-            bufferedWriter.write(tid_list[i]);
-            bufferedWriter.append(",");
-            bufferedWriter.write(tid_list[i + 1]);
-            bufferedWriter.append("\n");
-            // Add weight to each edge
-            nodeMatrix[tid_list[i]][tid_list[i + 1]]++;
-            nodeMatrix[tid_list[i]][tid_list[i]] = iniRank;
-        }
+        bufferedWriter.write(source[i]);
+        bufferedWriter.append(",");
+        bufferedWriter.write(target[i]);
+        bufferedWriter.append("\n");
+        // Add weight to each edge
+        nodeMatrix[source[i]][target[i]]++;
+        nodeMatrix[source[i]][source[i]] = iniRank;
     }
 }
 print("Creating node matrix...")
@@ -95,33 +121,34 @@ if (bufferedWriter != null) {
     print("Done.")
 }
 // Pagerank Algorithm
+//call this for each cpu separately //fixme
 print("Starting pageranking...")
 var out = new Array();
-for (i = 0; i < tids.length; i++) {
-    out[tids[i]] = 0;
-    for (j = 0; j < tids.length; j++) {
-        if (nodeMatrix[tids[i]][tids[j]] != 0 && tids[i] != tids[j]) {
-            out[tids[i]]++
+for (i = 0; i < tid_set.length; i++) {
+    out[tid_set[i]] = 0;
+    for (j = 0; j < tid_set.length; j++) {
+        if (nodeMatrix[tid_set[i]][tid_set[j]] != 0 && tid_set[i] != tid_set[j]) {
+            out[tid_set[i]] += nodeMatrix[tid_set[i]][tid_set[j]];
         }
     }
 }
-for (i = 0; i < tids.length; i++) {
+for (i = 0; i < tid_set.length; i++) {
     var deNom = 0;
-    for (j = 0; j < tids.length; j++) {
-        if (nodeMatrix[tids[j]][tids[i]] != 0 && tids[i] != tids[j]) {
-            deNom += out[tids[i]];
+    for (j = 0; j < tid_set.length; j++) {
+        if (nodeMatrix[tid_set[j]][tid_set[i]] != 0 && tid_set[i] != tid_set[j]) {
+            deNom += out[tid_set[i]];
         }
     }
-    nodeMatrix[tids[i]][tids[i]] = nodeMatrix[tids[i]][tids[i]] / deNom
-    print("TID: " + tids[i] + " has pagerank of " + nodeMatrix[tids[i]][tids[i]])
+    nodeMatrix[tid_set[i]][tid_set[i]] = nodeMatrix[tid_set[i]][tid_set[i]] / deNom
+    print("TID: " + tid_set[i] + " has pagerank of " + nodeMatrix[tid_set[i]][tid_set[i]])
 }
 print("Pagerank done.")
 var l = 0;
 var l_tid = 0;
-for (i = 0; i < tids.length; i++) {
-    if (nodeMatrix[tids[i]][tids[i]] > l) {
-        l = nodeMatrix[tids[i]][tids[i]]
-        l_tid = tids[i]
+for (i = 0; i < tid_set.length; i++) {
+    if ((nodeMatrix[tid_set[i]][tid_set[i]] > l) && isFinite(nodeMatrix[tid_set[i]][tid_set[i]])) {
+        l = nodeMatrix[tid_set[i]][tid_set[i]]
+        l_tid = tid_set[i]
     }
 }
 var filter = "TID==" + l_tid;
